00f0131a9cad158dd9daff845381b7d8f37283c6
blue-script <alexm92@inbox.ru>
2024-06-14 00:11:17 +0500

    add posts, create post

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <h4>Создание поста</h4>
+    <form @submit.prevent>
+      <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Название">
+      <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Описание">
+      <button class="btn" @click="createPost">Добавить</button>
+    </form>
+
+    <div class="post" v-for="post in posts">
+      <div><strong>Название</strong> {{ post.title }}</div>
+      <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'TypeScript', body: 'Описание поста'},
+        {id: 3, title: 'Vue', body: 'Описание поста'},
+      ],
+      title: '',
+      body: '',
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost)
+    },
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+form {
+  display: flex;
+  flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.btn {
+  margin-top: 15px;
+  align-self: flex-start;
+  padding: 5px 10px;
+  background: none;
+  color: navy;
+  border: 1px solid navy;
 }
+
+.post {
+  padding: 15px;
+  border: 2px solid navy;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  padding: 5px 10px;
+  margin-top: 10px;
+  border: 1px solid navy;
+}
+
 </style>
+
+<!--single style component-->
